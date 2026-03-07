c3076363808cc2315538068c7575f83519ad2411
Rahul Singh <rahulsingh936805@gmail.com>
2026-03-08 00:15:02 +0530

    fs module is included

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express();
-
+const fs = require("fs");
 app.use(express.json());
 
-let data = fs.readFile("data.txt","utf-8");
+let data = fs.readFileSync("data.txt","utf-8");
 
 app.get("/data", (req, res) => {
   res.send(data);
